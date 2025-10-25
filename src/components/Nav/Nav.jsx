dc9ab1ca9cd8b9f7499956b768d397e5cb0cbe4d
Denis <denispetrasso77@gmail.com>
2025-10-24 21:34:39 -0300

    Mejoras en la Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,18 +1,29 @@
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext/useCartContext";
+import { useState } from "react";
 import "./Nav.css";
 
 export const Nav = () => {
   const { getTotalItems } = useCartContext();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
 
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/discos">Discos</Link></li>
-        <li><Link to="/remeras">Remeras</Link></li>
+    <nav className="navbar">
+      <div className="nav-header">
+        <button className="nav-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+      </div>
+
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+        <li><Link to="/discos" onClick={closeMenu}>Discos</Link></li>
+        <li><Link to="/remeras" onClick={closeMenu}>Remeras</Link></li>
         <li>
-          <Link to="/carrito">Carrito</Link>
+          <Link to="/carrito" onClick={closeMenu}>Carrito</Link>
           {getTotalItems() > 0 && (
             <span className="in-cart">{getTotalItems()}</span>
           )}
